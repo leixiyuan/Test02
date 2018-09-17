e988437ba0697c63ce86ba54dcd9ce9e8d5eca0a
lxyGoose <34675573+lxyGoose@users.noreply.github.com>
2018-09-18 00:54:42 +0800

    初始化项目

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by l xy on 2018/9/18.
+//  Copyright © 2018年 l xy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
